4f3d63b9a31d553ea8d42b763c11a7aaf04cf448
Amir Abbasi <96338051+amirabbasi-1999@users.noreply.github.com>
2022-07-16 22:25:53 +0430

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+//Javacript for responsive navigation menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+//Javacript for video slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
+
+//Javacript for play music home
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  if (mySong.paused) {
+    mySong.play();
+    icon.src = "bg/pause.png";
+  } else {
+    mySong.pause();
+    icon.src = "bg/play.png";
+  }
+};
